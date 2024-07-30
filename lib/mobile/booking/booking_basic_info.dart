277dd5ffd37bb4908093e55b516bd439811cfe13
Muhammad Abdul Kaium <makaium33@gmail.com>
2024-07-31 00:59:12 +0600

    Congratulation Dialog added with superb responsiveness

--- lib/mobile/booking/booking_basic_info.dart
+++ lib/mobile/booking/booking_basic_info.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:stock_trading_app/common/common_button.dart';
+import 'package:stock_trading_app/common/custom_alart_dialog.dart';
 import 'package:stock_trading_app/common/custom_check_box.dart';
 import 'package:stock_trading_app/controller/booking_page_controller.dart';
 import 'package:stock_trading_app/controller/confirmation_controller.dart';
@@ -520,7 +521,95 @@ class BookingBasicInfo extends StatelessWidget {
                               ),
                             ),
                             onPressed: () {
-                              confirmationController.loadConfirmationPage();
+                              // confirmationController.loadConfirmationPage();
+                              Get.dialog(
+                                CustomAlartDialog(
+                                  end: -0.03,
+                                  horizontalPadding: screenWidth * 0.041666,
+                                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                  closeIcon: Padding(
+                                    padding: EdgeInsets.only(right: screenWidth * 0.03, top: screenWidth * 0.0215),
+                                    child: Icon(
+                                      Icons.close,
+                                      color: const Color(0xFF71717A),
+                                      size: screenWidth * 0.06,
+                                    ),
+                                  ),
+                                  onClose: () {
+                                    Future.delayed(const Duration(milliseconds: 300), 
+                                      () {
+                                        // signInAndSignUpController.toggleSignInSignUp();
+                                        // signInAndSignUpController.onClose();
+                                      }
+                                    );
+                                  },
+                                  dialogHeader: Padding(
+                                    padding: EdgeInsets.only(top: screenHeight * 0.0204,),
+                                    child: Text(
+                                      'Congratulations!',
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                        fontSize: 14.3.sp,
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.w600,
+                                        color: const Color(0xFF1D192B),
+                                        // overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ),
+                                  dialogContent: Padding(
+                                    padding: const EdgeInsets.only(left: 18, right: 18),
+                                    child: Column(
+                                      children: [
+                                        SizedBox(height: screenHeight * 0.0104,),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              'Your Booking is confirmed',
+                                              textAlign: TextAlign.justify,
+                                              style: TextStyle(
+                                                fontSize: 12.65.sp,
+                                                fontFamily: 'Poppins',
+                                                fontWeight: FontWeight.w400,
+                                                color: const Color(0xFF52525B),
+                                                // overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(height: screenHeight * 0.02,),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.end,
+                                          children: [
+                                            SizedBox(
+                                              width: screenWidth * 0.135,
+                                              height: screenHeight * 0.038,
+                                              child: CommonButton(
+                                                borderRadius: 8,
+                                                backgroundColor: const Color(0xFF008037),
+                                                child: Text('OK',
+                                                  style: TextStyle(
+                                                    fontSize: 11.5.sp,
+                                                    fontFamily: 'Gilroy',
+                                                    fontWeight: FontWeight.w500,
+                                                    overflow: TextOverflow.ellipsis,
+                                                  ),
+                                                ),
+                                                onPressed: () {
+                                                  Get.back();
+                                                  confirmationController.loadConfirmationPage();
+                                                },
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(height: screenHeight * 0.0192,),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                // barrierDismissible: false,
+                              );
                               // if(buyInfoFormkey.currentState!.validate()) {
                               //   bookingPageController.loadBookingPage();
                               //   Future.delayed(const Duration(milliseconds: 300), () {
